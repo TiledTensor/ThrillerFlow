5f18c11d9c319ba76ebcc1f8f53a7306171b4d34
kuangjux <18630816527@163.com>
2024-05-08 00:14:50 +0800

    fix RF Block example.

--- examples/rf_block.rs
+++ examples/rf_block.rs
@@ -18,22 +18,16 @@ fn main() {
     let s_c = Rc::new(Buffer::new("sC"));
     let out_edge = AttachedEdge::new(acc, s_c, None);
 
-    let iter_var1 = Rc::new(IterationVar::new("i1", (0, 10)));
-    let iter_var2 = Rc::new(IterationVar::new("i2", (0, 10)));
-    let iter_var3 = Rc::new(IterationVar::new("i3", (0, 10)));
+    let iter_var = Rc::new(IterationVar::new("kK", (0, 10)));
 
-    let mut access_map = AccessMap::new(3, vec![2, 2, 2]);
-    access_map.add_iter_var(iter_var1);
-    access_map.add_iter_var(iter_var2);
-    access_map.add_iter_var(iter_var3);
+    let mut access_map = AccessMap::new(1, vec![1]);
+    access_map.add_iter_var(iter_var);
 
-    access_map.add_access_matrix(AccessMatrix(vec![vec![1, 0, 0], vec![0, 0, 1]]));
-    access_map.add_access_matrix(AccessMatrix(vec![vec![0, 0, 1], vec![0, 1, 0]]));
-    access_map.add_access_matrix(AccessMatrix(vec![vec![1, 0, 0], vec![0, 1, 0]]));
+    access_map.add_access_matrix(AccessMatrix(vec![vec![1]]));
+    access_map.add_access_matrix(AccessMatrix(vec![vec![1]]));
 
-    access_map.add_access_offset(AccessOffset(vec![0, 1]));
-    access_map.add_access_offset(AccessOffset(vec![1, 0]));
-    access_map.add_access_offset(AccessOffset(vec![0, 0]));
+    access_map.add_access_offset(AccessOffset(vec![0]));
+    access_map.add_access_offset(AccessOffset(vec![0]));
 
     let access_map = Rc::new(access_map);
 
