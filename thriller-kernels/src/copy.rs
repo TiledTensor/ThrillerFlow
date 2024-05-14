a1b23ff4d6a5bc69e0d9c44dd60c64a7721f19c1
kuangjux <18630816527@163.com>
2024-05-14 19:38:19 +0800

    feat: Add thriller-kernel library.

---
+++ thriller-kernels/src/copy.rs
@@ -0,0 +1,20 @@
+/// Copy Primitive.
+pub struct Copy;
+
+impl Copy {
+    /// Emit a copy from a global buffer to a shared buffer.
+    pub fn emit_copy_2d_tile_g2s<T: AsRef<str>>(
+        src: T,
+        src_index: usize,
+        dst: T,
+        dst_index: usize,
+    ) -> String {
+        format!(
+            "copy_2d_tile_g2s({src}[{src_index}], {dst}[{dst_index}])",
+            src = src.as_ref(),
+            src_index = src_index,
+            dst = dst.as_ref(),
+            dst_index = dst_index
+        )
+    }
+}
---
+++ thriller-kernels/src/lib.rs
@@ -0,0 +1,8 @@
+//! `thriller-kernels` is a Rust wrapper for Macro Kernels along with an API interface.
+
+#![deny(warnings)]
+#![deny(missing_docs)]
+
+mod copy;
+
+pub use copy::Copy;
