6ab21b4c652bdd5ba7e85f6faea8c6d7f6b64e3f
kuangjux <18630816527@163.com>
2024-05-15 12:22:13 +0800

    feat(engine): Add install tiledcuda method.

--- thriller-core/src/engine/mod.rs
+++ thriller-core/src/engine/mod.rs
@@ -1,4 +1,7 @@
-use std::{fs::File, io::Write};
+use std::fs;
+use std::io::Write;
+use std::process::Command;
+use std::{env::temp_dir, fs::File};
 
 use crate::{ThrillerBlock, ThrillerError, ThrillerResult};
 
@@ -32,7 +35,33 @@ impl ThrillerEngine {
     }
 
     /// Install macro kernel for the given dataflow block.
-    pub fn install_macro_kernels(&self) -> ThrillerResult<()> {
-        todo!()
+    pub fn install_library(&self) -> ThrillerResult<()> {
+        let temp_dir = temp_dir().join("thriller");
+        fs::create_dir(temp_dir.clone()).map_err(|_| ThrillerError::FailedFileOp)?;
+
+        std::env::set_current_dir(&temp_dir).map_err(|_| ThrillerError::FailedFileOp)?;
+
+        let clone = Command::new("git")
+            .args(["clone", "git@github.com:TiledTensor/TiledCUDA.git"])
+            .output()
+            .map_err(|_| ThrillerError::FailedFileOp)?;
+
+        if !clone.status.success() {
+            return Err(ThrillerError::FailedFileOp);
+        }
+
+        let repo_dir = temp_dir.join("TiledCUDA");
+        std::env::set_current_dir(repo_dir).map_err(|_| ThrillerError::FailedFileOp)?;
+
+        let submodule = Command::new("git")
+            .args(["submodule", "update", "--init", "--recursive"])
+            .output()
+            .map_err(|_| ThrillerError::FailedFileOp)?;
+
+        if !submodule.status.success() {
+            return Err(ThrillerError::FailedFileOp);
+        }
+
+        Ok(())
     }
 }
